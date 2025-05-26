7dd4c6c20414bc3666dbb390097bcd7199d536c6
Tom Sydney Kerckhove <syd@cs-syd.eu>
2025-05-26 12:43:27 +0200

    Drop really old backward compatibility

--- flake.nix
+++ flake.nix
@@ -5,10 +5,6 @@
     nixpkgs-24_11.url = "github:NixOS/nixpkgs?ref=nixos-24.11";
     nixpkgs-24_05.url = "github:NixOS/nixpkgs?ref=nixos-24.05";
     nixpkgs-23_11.url = "github:NixOS/nixpkgs?ref=nixos-23.11";
-    nixpkgs-23_05.url = "github:NixOS/nixpkgs?ref=nixos-23.05";
-    nixpkgs-22_11.url = "github:NixOS/nixpkgs?ref=nixos-22.11";
-    nixpkgs-22_05.url = "github:NixOS/nixpkgs?ref=nixos-22.05";
-    nixpkgs-21_11.url = "github:NixOS/nixpkgs?ref=nixos-21.11";
     pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
   };
 
@@ -18,10 +14,6 @@
     , nixpkgs-24_11
     , nixpkgs-24_05
     , nixpkgs-23_11
-    , nixpkgs-23_05
-    , nixpkgs-22_11
-    , nixpkgs-22_05
-    , nixpkgs-21_11
     , pre-commit-hooks
     }:
     let
@@ -46,11 +38,7 @@
             inherit
               nixpkgs-24_11
               nixpkgs-24_05
-              nixpkgs-23_11
-              nixpkgs-23_05
-              nixpkgs-22_11
-              nixpkgs-22_05
-              nixpkgs-21_11;
+              nixpkgs-23_11;
           };
           backwardCompatibilityChecks = pkgs.lib.mapAttrs (_: nixpkgs: backwardCompatibilityCheckFor nixpkgs) allNixpkgs;
         in
